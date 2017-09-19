c62383134406598820f8d0934cab7bf83af1efa9
Svante v. Erichsen <svante.v.erichsen@web.de>
2017-09-20 01:43:46 +0200

    Nominelle Mannschaftsstärke berechnen

--- mannschaft.lisp
+++ mannschaft.lisp
@@ -6,13 +6,21 @@
    (kuerzel :initarg :kuerzel
             :reader mannschaft-kuerzel)
    (spieler :initform (make-array 10 :fill-pointer 0)
-            :reader mannschaft-spieler)))
+            :reader mannschaft-spieler)
+   (staerke :accessor mannschaft-staerke)))
 
 (defmethod parse-event ((event-name (eql 'mannschaft)) &rest args)
   (destructuring-bind (kuerzel name &rest spieler-data) args
     (let ((mannschaft (make-instance 'mannschaft
                                      :kuerzel kuerzel
                                      :name name)))
-      (dolist (spieler-datum spieler-data mannschaft)
+      (dolist (spieler-datum spieler-data)
         (vector-push-extend (parse-spieler spieler-datum)
-                            (mannschaft-spieler mannschaft))))))
+                            (mannschaft-spieler mannschaft)))
+      (setf (mannschaft-staerke mannschaft)
+            (-<> (mannschaft-spieler mannschaft)
+                 (subseq 0 5)
+                 (map 'vector #'spieler-rang <>)
+                 (map 'vector #'rang-int <>)
+                 (reduce #'+ <>)))
+      mannschaft)))
